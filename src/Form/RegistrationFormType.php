589fc0ad6c0a0c48b7e5167ab0c13b20feb5fb3c
Miunn <remcaulier@orange.fr>
2023-10-14 21:34:14 +0200

    Css on register form and messages

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -26,15 +26,6 @@ class RegistrationFormType extends AbstractType
             ->add('lastname', TextType::class, [
                 'label' => 'user.lastname'
             ])
-            ->add('lastname')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
