58f44d804d17cfd3234565262840c95cdb82bff0
Miunn <remcaulier@orange.fr>
2023-10-15 12:23:23 +0200

    Redirect to app index instead of core index on register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -34,7 +34,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_core_index');
+            return $this->redirectToRoute('app_app_index');
         }
 
         return $this->render('registration/register.twig', [
