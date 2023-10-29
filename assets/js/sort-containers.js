e6b76f3d87f128146c4b549f75d28f9b05ce2d96
Miunn <remcaulier@orange.fr>
2023-10-29 12:07:36 +0100

    Sorting container on state, straight and reverse

--- assets/js/sort-containers.js
+++ assets/js/sort-containers.js
@@ -14,19 +14,37 @@ function sortByName(containers, reverse=false) {
     }
 }
 
+function sortByState(containers, reverse=false) {
+    let order = ['Running', 'Paused', 'Restarting', 'Exited', 'Created', 'Removing', 'Dead'];
+    if (reverse) {
+        return containers.sort((a, b) => {
+            let aState = a.querySelector('.state').innerText.trim();
+            let bState = b.querySelector('.state').innerText.trim();
+
+            return order.indexOf(bState) - order.indexOf(aState);
+        });
+    } else {
+        return containers.sort((a, b) => {
+            let aState = a.querySelector('.state').innerText.trim();
+            let bState = b.querySelector('.state').innerText.trim();
+
+            return order.indexOf(aState) - order.indexOf(bState);
+        });
+    }
+}
+
 const sortName = document.getElementById('sort-name');
 const sortState = document.getElementById('sort-state');
 
 const containersParent = document.getElementById(sortName.dataset.wrapper);
-const containers = containersParent.children;
-console.log(containers);
+const containers = Array.from(containersParent.children);
 
 sortName.addEventListener('click', (e) => {
     e.preventDefault();
 
     let performReverse = e.currentTarget.dataset.reverse === "true";
     e.currentTarget.setAttribute('data-reverse', performReverse === false);
-    let sorted = sortByName(Array.from(containers), performReverse);
+    let sorted = sortByName(containers, performReverse);
 
     while (containersParent.firstChild) {
         containersParent.removeChild(containersParent.lastChild);
@@ -41,4 +59,16 @@ sortState.addEventListener('click', (e) => {
     e.preventDefault();
 
     console.log('sort state');
+    let performReverse = e.currentTarget.dataset.reverse === "true";
+    e.currentTarget.setAttribute('data-reverse', performReverse === false);
+    let sorted = sortByState(containers, performReverse);
+    console.log(sorted)
+
+    while (containersParent.firstChild) {
+        containersParent.removeChild(containersParent.lastChild);
+    }
+
+    for (let container of sorted) {
+        containersParent.appendChild(container);
+    }
 });
