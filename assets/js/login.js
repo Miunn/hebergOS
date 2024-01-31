7b8667fae6b8e587ff9b3df1ca20a582e8a6c664
Miunn <remcaulier@orange.fr>
2024-01-31 22:00:47 +0100

    Spotlight effect

--- assets/js/login.js
+++ assets/js/login.js
@@ -25,10 +25,29 @@ const computerAscii = `
 `
 const computerContainer = document.getElementById("computer");
 
-for (let c of computerAscii) {
-    computerContainer.innerText += c;
+let computerAnim = async () => {
+    for (let c of computerAscii) {
+        computerContainer.innerText += c;
 
-    if (c !== " ") {
-        await new Promise(r => setTimeout(r, 5));
+        if (c !== " ") {
+            await new Promise(r => setTimeout(r, 1));
+        }
     }
-}
+};
+
+computerAnim();
+
+// Spotlight effect
+// Map mouse position to css vars
+const r = document.querySelector(':root');
+document.querySelectorAll(".card-spotlight")
+    .forEach(card => {
+        card.addEventListener("mousemove", (e) => {
+            const rect = card.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            card.style.setProperty("--mouse-x", `${x}px`);
+            card.style.setProperty("--mouse-y", `${y}px`);
+        });
+    });
