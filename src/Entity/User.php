328c6f38d01392ba485a1ff667fc263f40da514d
Miunn <remi@remicaulier.fr>
2025-01-10 14:47:25 +0100

    Orphan removal false user containers

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -34,7 +34,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\ManyToMany(targetEntity: Containers::class, mappedBy: 'user', cascade: ['persist'])]
+    #[ORM\ManyToMany(targetEntity: Containers::class, mappedBy: 'user', cascade: ['persist'], orphanRemoval: false)]
     private Collection $containers;
 
     #[ORM\Column(length: 25)]
