79f6767c4811d65164e9caa02ae1d5c2edd77234
Miunn <remi@remicaulier.fr>
2025-01-27 12:43:17 +0100

    Container component

--- src/app/[locale]/app/page.tsx
+++ src/app/[locale]/app/page.tsx
@@ -1,5 +1,7 @@
 import getMe from "@/actions/user"
+import Container from "@/components/Container";
 import { getTranslations } from "next-intl/server";
+import { Fragment } from "react";
 
 export default async function Dashboard() {
 
@@ -7,12 +9,12 @@ export default async function Dashboard() {
     const me = await getMe();
 
     return (
-        <main className="max-w-7xl mx-auto md:h-screen">
-            <h1 className="font-semibold text-3xl mt-14">{t('title')}</h1>
+        <main className="max-w-7xl mx-auto px-24 md:h-screen">
+            <h1 className="font-semibold text-3xl mt-14 mb-7">{t('title')}</h1>
             {me?.containers.map((container) => (
-                <div key={container.id}>
-                    <h1>{container.name}</h1>
-                </div>
+                <Fragment key={container.id}>
+                    <Container id={container.id} name={container.name} state={"exited"} />
+                </Fragment>
             ))}
 
             {me?.containers.length === 0
