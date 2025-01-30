6195abf0532dd14235b1a551d515537deaa2d44b
Miunn <remi@remicaulier.fr>
2025-01-30 17:09:05 +0100

    Containers table sorting

--- src/components/tables/containers-columns.tsx
+++ src/components/tables/containers-columns.tsx
@@ -4,6 +4,8 @@ import { Container } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import { Checkbox } from "../ui/checkbox";
 import { useTranslations } from "next-intl";
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "../ui/button";
 
 export const containersColumns: ColumnDef<Container>[] = [
     {
@@ -30,31 +32,58 @@ export const containersColumns: ColumnDef<Container>[] = [
     },
     {
         accessorKey: "name",
-        header: () => {
+        header: ({ column }) => {
             const t = useTranslations("tables.containers.columns");
 
-            return t('name');
+            return <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                {t('name')}
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
         }
     },
     {
         accessorKey: "state",
-        header: () => {
+        header: ({ column }) => {
             const t = useTranslations("tables.containers.columns");
-            return t('state');
+
+            return <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                {t('state')}
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
         }
     },
     {
         accessorKey: "hostPort",
-        header: () => {
+        header: ({ column }) => {
             const t = useTranslations("tables.containers.columns");
-            return t('hostPort');
+
+            return <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                {t('hostPort')}
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
         }
     },
     {
         accessorKey: "memory",
-        header: () => {
+        header: ({ column }) => {
             const t = useTranslations("tables.containers.columns");
-            return t('memory');
+
+            return <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                {t('memory')}
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
         },
         cell: ({ row }) => {
             const memory = row.original.memory;
@@ -63,9 +92,16 @@ export const containersColumns: ColumnDef<Container>[] = [
     },
     {
         accessorKey: "cpu",
-        header: () => {
+        header: ({ column }) => {
             const t = useTranslations("tables.containers.columns");
-            return t('cpu');
+
+            return <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                {t('cpu')}
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
         },
         cell: ({ row }) => {
             const cpu = row.original.cpu;
--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -4,6 +4,8 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
   useReactTable,
 } from "@tanstack/react-table"
 
@@ -15,6 +17,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { useState } from "react"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -25,10 +28,17 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    }
   })
 
   return (
