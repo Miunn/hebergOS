75d6d5ddf9ef6e6cbd323419b97385e6ecd9e0bf
Miunn <remi@remicaulier.fr>
2025-02-02 16:57:06 +0100

    Remove logging

--- src/app/api/users/check-container/route.ts
+++ src/app/api/users/check-container/route.ts
@@ -4,7 +4,6 @@ import { isAdmin } from "@/lib/utils";
 
 export async function POST(request: NextRequest) {
     if (!request.nextUrl.searchParams.get("containerId")) {
-        console.log("Container ID not found in query");
         return new Response(null, { status: 400 });
     }
 
@@ -17,15 +16,12 @@ export async function POST(request: NextRequest) {
     const user = await prisma.user.findUnique({ where: { id: body.user }, include: { containers: true } });
 
     if (!user) {
-        console.log("User not found to authorize to container");
         return new Response(null, { status: 401 });
     }
 
     if ((await isAdmin()) || user.containers.map(c => c.id).includes(request.nextUrl.searchParams.get("containerId")!)) {
-        console.log("Authorized to container");
         return new Response(JSON.stringify({ status: true }), { status: 200 });
     }
 
-    console.log("Not authorized to container");
     return new Response(null, { status: 401 });
 }
