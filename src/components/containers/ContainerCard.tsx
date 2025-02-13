72f0ece81caaddaaebe47bd3d87898692cfb9323
Miunn <remi@remicaulier.fr>
2025-02-13 18:37:20 +0100

    Remove phantom link on landing page container card

--- src/components/containers/ContainerCard.tsx
+++ src/components/containers/ContainerCard.tsx
@@ -4,7 +4,7 @@ import { ContainerState } from "@prisma/client";
 import Link from "next/link";
 
 export default function Container({ className, id, name, state }: { className?: string, id?: string, name: string, state: ContainerState }) {
-    
+
     const getStateColor = (state: ContainerState) => {
         switch (state) {
             case "CREATED":
@@ -20,16 +20,32 @@ export default function Container({ className, id, name, state }: { className?: 
             default:
                 return "bg-gray-400";
         }
-      }
-    
-    return (
-        <Link href={id ? `/app/containers/${id}` : '#'} className={cn("flex flex-col justify-around w-52 md:w-60 h-24 border rounded-2xl p-4 transition-all duration-200 cursor-pointer bg-white hover:bg-gray-100 hover:shadow-md hover:translate-x-[2px] hover:translate-y-[-3px]", className)}>
-            <h2 className={`${robotoMono.className} antialiased truncate text-lg`}>{name}</h2>
+    }
 
-            <div className="flex items-center gap-2 capitalize">
-                <div className={`w-4 h-4 ${getStateColor(state)} rounded-full`} />
-                <p>{state.toLocaleLowerCase()}</p>
-            </div>
-        </Link>
+    return (
+        <>
+            {id
+                ? (
+                    <Link href={`/app/containers/${id}`} className={cn("flex flex-col justify-around w-52 md:w-60 h-24 border rounded-2xl p-4 transition-all duration-200 cursor-pointer bg-white hover:bg-gray-100 hover:shadow-md hover:translate-x-[2px] hover:translate-y-[-3px]", className)}>
+                        <h2 className={`${robotoMono.className} antialiased truncate text-lg`}>{name}</h2>
+
+                        <div className="flex items-center gap-2 capitalize">
+                            <div className={`w-4 h-4 ${getStateColor(state)} rounded-full`} />
+                            <p>{state.toLocaleLowerCase()}</p>
+                        </div>
+                    </Link>
+                )
+                : (
+                    <div className={cn("flex flex-col justify-around w-52 md:w-60 h-24 border rounded-2xl p-4 transition-all duration-200 cursor-pointer bg-white hover:bg-gray-100 hover:shadow-md hover:translate-x-[2px] hover:translate-y-[-3px]", className)}>
+                        <h2 className={`${robotoMono.className} antialiased truncate text-lg`}>{name}</h2>
+
+                        <div className="flex items-center gap-2 capitalize">
+                            <div className={`w-4 h-4 ${getStateColor(state)} rounded-full`} />
+                            <p>{state.toLocaleLowerCase()}</p>
+                        </div>
+                    </div>
+                )
+            }
+        </>
     )
 }
