c3b68f0b77417799d5da9aa256c0c7259c47e6a4
Miunn <remi@remicaulier.fr>
2025-02-14 15:35:21 +0100

    Login form responsive. Remove Forgot password link for now on

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -10,7 +10,7 @@ export default async function LoginPage() {
   return (
     <>
       <Header locale={locale} />
-      <main className="flex items-center justify-center md:h-screen">
+      <main className="flex items-center justify-center h-screen">
         <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
           <Suspense>
             <LoginForm />
